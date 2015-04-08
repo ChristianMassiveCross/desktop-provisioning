0d5064d0a21321a38c7c49362278e49322563488
Oolong Brothers <oolongbrothers@gmx.net>
2015-04-09 00:14:54 +0200

    Removes include from boostrap script in favor of inlineing common commands

--- boostrap/bootstrap-ubuntu.sh
+++ boostrap/bootstrap-ubuntu.sh
@@ -4,4 +4,14 @@ echo disable > /sys/firmware/acpi/interrupts/gpe66
 sudo apt-get update
 sudo apt-get install git python-pip python-yaml python-jinja2
 
-source includes/common.sh
+#common part
+sudo pip install ansible==1.9.0.1
+git config --global user.name "Oolong Brothers"
+git config --global user.email oolongbrothers@gmx.net
+git config --global push.default simple
+cd ~
+mkdir Code
+cd Code
+git clone git@github.com:oolongbrothers/desktop-provisioning.git
+cd desktop-provisioning
+
--- boostrap/includes/common.sh
+++
@@ -1,9 +0,0 @@
-sudo pip install ansible==1.9.0.1
-git config --global user.name "Oolong Brothers"
-git config --global user.email oolongbrothers@gmx.net
-git config --global push.default simple
-cd ~
-mkdir Code
-cd Code
-git clone git@github.com:oolongbrothers/desktop-provisioning.git
-cd desktop-provisioning

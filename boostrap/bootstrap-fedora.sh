0054edce0809819b04349792d6213045ffd96694
Oolong Brothers <oolongbrothers@gmx.net>
2015-05-03 16:51:41 +0200

    Improves fedora bootstrap script: - Turns off deltarpms first - Adds interactivity - Improves messages

--- boostrap/bootstrap-fedora.sh
+++ boostrap/bootstrap-fedora.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Make sure you have put in ssh keys before running this"
-echo disable > /sys/firmware/acpi/interrupts/gpe66
-sudo yum update
-sudo yum install python-pip python-yaml python-jinja2 python-crypto
+read -p "Press [Enter] key to start boostrap process..."
+sudo su -c "chown 'deltarpm=0' >> /etc/yum.conf"
+sudo yum update -y
+sudo yum install -y python-pip python-yaml python-jinja2 python-crypto
 
 #common part
 sudo pip install ansible==1.9.0.1
@@ -14,4 +15,5 @@ mkdir Code
 cd Code
 git clone git@github.com:oolongbrothers/desktop-provisioning.git
 cd desktop-provisioning
-
+echo "If you have any, put your custom localhost.yaml host_vars file into ~/Code/desktop-provisoning/inventories/host_vars/localhost.yaml"
+echo "Then run: cd ~/Code/desktop-provisoning; ansible-playbook desktop-provisioning.yaml -i inventories/inventory --diff --ask-sudo-pass"

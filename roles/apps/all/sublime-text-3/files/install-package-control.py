5923a45b0a3befa9f0016627833e071ef9aa8c7c
Oolong Brothers <oolongbrothers@gmx.net>
2015-10-26 00:44:50 +0100

    sublime-text: Fixes package control installation

--- roles/apps/all/sublime-text-3/files/install-package-control.py
+++ roles/apps/all/sublime-text-3/files/install-package-control.py
@@ -2,11 +2,17 @@
 import urllib.request
 import os
 import hashlib
+import sys
 
-h = 'eb2297e1a458f27d836c04bb0cbaf282' + 'd0e7a3098092775ccb37ca9d6b2e4b7d'
+h = '2915d1851351e5ee549c20394736b442' + '8bc59f460fa1548d1514676163dafc88'
 pf = 'Package Control.sublime-package'
 ipp = os.getenv("HOME") + '/.config/sublime-text-3/Installed Packages'
 urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) )
 by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read()
 dh = hashlib.sha256(by).hexdigest()
-print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
+if dh != h:
+    print('Error validating download (got %s instead of %s), please try manual install' % (dh, h))
+    sys.exit(1)
+else:
+    open(os.path.join( ipp, pf), 'wb' ).write(by)
+    sys.exit(0)
